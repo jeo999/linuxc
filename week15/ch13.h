bba4879e33974c0e2da9285df950fa72198e420f
jeo999 <997435449@qq.com>
2019-12-16 05:02:04 -0800

    15-2

---
+++ week15/ch13.h
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<pthread.h>
+#include<string.h>
+#include<errno.h>
+#include<sys/tpes.h>
---
+++ week15/my.h
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<pthread.h>
+#include<string.h>
+#include<errno.h>
+#include<sys/tpes.h>
+
+#define LOOP 10000000
+#define NUM 4
+#define NT 100
+#define WRITE_FIRST
